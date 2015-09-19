082d6e6bdec52c93b4bf6f32fdbee904cb3fa2ba
heidikumar <heidi.kristine.hansen@gmail.com>
2015-09-18 20:14:13 -0700

    Added redirect after click shorten

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -8,7 +8,9 @@ angular.module('shortly.shorten', [])
     var newLink = $scope.newLink;
     Links.postLink(newLink).then(function (resp) {
       console.log('Go to Disneyland!');
-      $location.href = 'http://localhost:3000/#/links';
+      // $location.path = 'http://localhost:3000/#/links';
+      $location.path('/links'); //Redirect to /links
+
       return resp;
     });
   };
